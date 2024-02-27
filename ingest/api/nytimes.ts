87d00a1b56bd4e0f1871a94579c042976bb98f8c
Peter Aleksa <peter.aleksa@gmail.com>
2024-02-27 11:55:56 -0500

    fixes model rename

--- ingest/api/nytimes.ts
+++ ingest/api/nytimes.ts
@@ -1,17 +1,17 @@
-import { nyt_api_topstories_document } from '../../models/ts/nytimes/NYTimesApiTopStoriesDocument';
+import { NYTimesApiTopStoriesDocument } from '../../models/ts/nytimes/NYTimesApiTopStoriesDocument';
 
 const apiKey = process.env.NYT_API_KEY;
 
 /**
  *
  */
-export async function getNYTimesTopStories(): Promise<Array<nyt_api_topstories_document>> {
+export async function getNYTimesTopStories(): Promise<Array<NYTimesApiTopStoriesDocument>> {
     let res = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`);
     if (res.status === 401) throw new Error("Unauthorized request. Make sure api-key is set.");
     if (res.status === 429) throw new Error("Too many requests. You reached your per minute or per day rate limit.")
     if (res.status !== 200) throw new Error();
     let resJson = await res.json();
-    return resJson.results as Array<nyt_api_topstories_document>;
+    return resJson.results as Array<NYTimesApiTopStoriesDocument>;
 }
 
 /**
@@ -43,7 +43,7 @@ function _parseAuthorsFromNYTimesByline(byline: string) {
  *
  * @param topStory
  */
-export function mapNYTimesTopStoryToArticle(topStory: nyt_api_topstories_document) {
+export function mapNYTimesTopStoryToArticle(topStory: NYTimesApiTopStoriesDocument) {
     return  {
         abstract: topStory.abstract,
         author: _parseAuthorsFromNYTimesByline(topStory.byline),
