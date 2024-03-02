4b439676f1ceff2df7a78f7a3c0008250de55818
Peter Aleksa <peter.aleksa@gmail.com>
2024-03-01 19:05:33 -0500

    fixed types

--- models/ts/resolvers-types.ts
+++ models/ts/resolvers-types.ts
@@ -1,4 +1,4 @@
-import { GraphQLResolveInfo } from 'graphql';
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -14,6 +14,8 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
+  Date: { input: string; output: string; }
+  DateTime: { input: string; output: string; }
 };
 
 export type AdvertisingInfo = {
@@ -46,6 +48,7 @@ export type Belief = GraphNode & {
   name: Scalars['String']['output'];
   similar_beliefs: Array<Belief>;
   summary: Scalars['String']['output'];
+  wiki_tag?: Maybe<Scalars['String']['output']>;
 };
 
 export type Comment = GraphNode & {
@@ -55,11 +58,11 @@ export type Comment = GraphNode & {
   platform: MediaChannel;
   text: Scalars['String']['output'];
   title?: Maybe<Scalars['String']['output']>;
-  username: Scalars['String']['output'];
+  user: User;
 };
 
 export type DistributionInfo = {
-  date: Scalars['Int']['output'];
+  date: Scalars['DateTime']['output'];
   url: Scalars['String']['output'];
 };
 
@@ -95,6 +98,8 @@ export type MediaChannel = GraphNode & {
   id: Scalars['ID']['output'];
   name: Scalars['String']['output'];
   owners: Array<Entity>;
+  users: Array<User>;
+  wiki_tag?: Maybe<Scalars['String']['output']>;
 };
 
 export type MentionInfo = {
@@ -112,22 +117,29 @@ export type Organization = GraphNode & {
   name: Scalars['String']['output'];
   subsidiaries: Array<Organization>;
   subsidiary_of: Array<Organization>;
+  wiki_tag?: Maybe<Scalars['String']['output']>;
 };
 
 export type OwnershipInfo = {
   details?: Maybe<Scalars['String']['output']>;
+  end_dt?: Maybe<Scalars['DateTime']['output']>;
+  start_dt?: Maybe<Scalars['DateTime']['output']>;
 };
 
 export type Person = GraphNode & {
   __typename?: 'Person';
+  affiliations: Array<Organization>;
+  aka?: Maybe<Array<Maybe<Scalars['String']['output']>>>;
+  bio?: Maybe<Scalars['String']['output']>;
+  dob?: Maybe<Scalars['Date']['output']>;
   education: Array<Organization>;
   emails?: Maybe<Array<Maybe<Scalars['String']['output']>>>;
   employment: Array<Organization>;
-  first_name: Scalars['String']['output'];
   id: Scalars['ID']['output'];
   investments: Array<Asset>;
-  last_name: Scalars['String']['output'];
-  middle_name?: Maybe<Scalars['String']['output']>;
+  name: Scalars['String']['output'];
+  user_accounts: Array<User>;
+  wiki_tag?: Maybe<Scalars['String']['output']>;
 };
 
 export type PoliticalPosition = GraphNode & {
@@ -137,10 +149,24 @@ export type PoliticalPosition = GraphNode & {
   name: Scalars['String']['output'];
   similar_positions: Array<PoliticalPosition>;
   summary: Scalars['String']['output'];
+  wiki_tag?: Maybe<Scalars['String']['output']>;
 };
 
 export type SimilarityInfo = {
   details?: Maybe<Scalars['String']['output']>;
+};
+
+export type User = GraphNode & {
+  __typename?: 'User';
+  id: Scalars['ID']['output'];
+  owner: Array<Entity>;
+  platform: Array<MediaChannel>;
+  username: Scalars['String']['output'];
+};
+
+export type UserInfo = {
+  details?: Maybe<Scalars['String']['output']>;
+  user_since?: Maybe<Scalars['DateTime']['output']>;
 };
 
 export type WithIndex<TObject> = TObject & Record<string, any>;
@@ -224,11 +250,12 @@ export type ResolversInterfaceTypes<RefType extends Record<string, unknown>> = R
   DistributionInfo: never;
   EducationInfo: never;
   EmploymentInfo: never;
-  GraphNode: ( Omit<Article, 'mentions'> & { mentions: Array<RefType['Entity']> } ) | ( Belief ) | ( Comment ) | ( Omit<MediaChannel, 'owners'> & { owners: Array<RefType['Entity']> } ) | ( Omit<Organization, 'investments'> & { investments: Array<RefType['Asset']> } ) | ( Omit<Person, 'investments'> & { investments: Array<RefType['Asset']> } ) | ( PoliticalPosition );
+  GraphNode: ( Omit<Article, 'mentions'> & { mentions: Array<RefType['Entity']> } ) | ( Belief ) | ( Comment ) | ( Omit<MediaChannel, 'owners'> & { owners: Array<RefType['Entity']> } ) | ( Omit<Organization, 'investments'> & { investments: Array<RefType['Asset']> } ) | ( Omit<Person, 'investments'> & { investments: Array<RefType['Asset']> } ) | ( PoliticalPosition ) | ( Omit<User, 'owner'> & { owner: Array<RefType['Entity']> } );
   InvestmentInfo: never;
   MentionInfo: never;
   OwnershipInfo: never;
   SimilarityInfo: never;
+  UserInfo: never;
 }>;
 
 /** Mapping between all available schema types and the resolvers types */
@@ -240,6 +267,8 @@ export type ResolversTypes = ResolversObject<{
   Belief: ResolverTypeWrapper<Belief>;
   Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
   Comment: ResolverTypeWrapper<Comment>;
+  Date: ResolverTypeWrapper<Scalars['Date']['output']>;
+  DateTime: ResolverTypeWrapper<Scalars['DateTime']['output']>;
   DistributionInfo: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['DistributionInfo']>;
   EducationInfo: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['EducationInfo']>;
   EmploymentInfo: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['EmploymentInfo']>;
@@ -257,6 +286,8 @@ export type ResolversTypes = ResolversObject<{
   PoliticalPosition: ResolverTypeWrapper<PoliticalPosition>;
   SimilarityInfo: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['SimilarityInfo']>;
   String: ResolverTypeWrapper<Scalars['String']['output']>;
+  User: ResolverTypeWrapper<Omit<User, 'owner'> & { owner: Array<ResolversTypes['Entity']> }>;
+  UserInfo: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['UserInfo']>;
 }>;
 
 /** Mapping between all available schema types and the resolvers parents */
@@ -268,6 +299,8 @@ export type ResolversParentTypes = ResolversObject<{
   Belief: Belief;
   Boolean: Scalars['Boolean']['output'];
   Comment: Comment;
+  Date: Scalars['Date']['output'];
+  DateTime: Scalars['DateTime']['output'];
   DistributionInfo: ResolversInterfaceTypes<ResolversParentTypes>['DistributionInfo'];
   EducationInfo: ResolversInterfaceTypes<ResolversParentTypes>['EducationInfo'];
   EmploymentInfo: ResolversInterfaceTypes<ResolversParentTypes>['EmploymentInfo'];
@@ -285,6 +318,8 @@ export type ResolversParentTypes = ResolversObject<{
   PoliticalPosition: PoliticalPosition;
   SimilarityInfo: ResolversInterfaceTypes<ResolversParentTypes>['SimilarityInfo'];
   String: Scalars['String']['output'];
+  User: Omit<User, 'owner'> & { owner: Array<ResolversParentTypes['Entity']> };
+  UserInfo: ResolversInterfaceTypes<ResolversParentTypes>['UserInfo'];
 }>;
 
 export type AdvertisingInfoResolvers<ContextType = any, ParentType extends ResolversParentTypes['AdvertisingInfo'] = ResolversParentTypes['AdvertisingInfo']> = ResolversObject<{
@@ -320,6 +355,7 @@ export type BeliefResolvers<ContextType = any, ParentType extends ResolversParen
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   similar_beliefs?: Resolver<Array<ResolversTypes['Belief']>, ParentType, ContextType>;
   summary?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  wiki_tag?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
@@ -329,13 +365,21 @@ export type CommentResolvers<ContextType = any, ParentType extends ResolversPare
   platform?: Resolver<ResolversTypes['MediaChannel'], ParentType, ContextType>;
   text?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   title?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  username?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  user?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
+export interface DateScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Date'], any> {
+  name: 'Date';
+}
+
+export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
+  name: 'DateTime';
+}
+
 export type DistributionInfoResolvers<ContextType = any, ParentType extends ResolversParentTypes['DistributionInfo'] = ResolversParentTypes['DistributionInfo']> = ResolversObject<{
   __resolveType: TypeResolveFn<null, ParentType, ContextType>;
-  date?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  date?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
   url?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
 }>;
 
@@ -358,7 +402,7 @@ export type EntityResolvers<ContextType = any, ParentType extends ResolversParen
 }>;
 
 export type GraphNodeResolvers<ContextType = any, ParentType extends ResolversParentTypes['GraphNode'] = ResolversParentTypes['GraphNode']> = ResolversObject<{
-  __resolveType: TypeResolveFn<'Article' | 'Belief' | 'Comment' | 'MediaChannel' | 'Organization' | 'Person' | 'PoliticalPosition', ParentType, ContextType>;
+  __resolveType: TypeResolveFn<'Article' | 'Belief' | 'Comment' | 'MediaChannel' | 'Organization' | 'Person' | 'PoliticalPosition' | 'User', ParentType, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
 }>;
 
@@ -376,6 +420,8 @@ export type MediaChannelResolvers<ContextType = any, ParentType extends Resolver
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   owners?: Resolver<Array<ResolversTypes['Entity']>, ParentType, ContextType>;
+  users?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType>;
+  wiki_tag?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
@@ -394,23 +440,30 @@ export type OrganizationResolvers<ContextType = any, ParentType extends Resolver
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   subsidiaries?: Resolver<Array<ResolversTypes['Organization']>, ParentType, ContextType>;
   subsidiary_of?: Resolver<Array<ResolversTypes['Organization']>, ParentType, ContextType>;
+  wiki_tag?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
 export type OwnershipInfoResolvers<ContextType = any, ParentType extends ResolversParentTypes['OwnershipInfo'] = ResolversParentTypes['OwnershipInfo']> = ResolversObject<{
   __resolveType: TypeResolveFn<null, ParentType, ContextType>;
   details?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  end_dt?: Resolver<Maybe<ResolversTypes['DateTime']>, ParentType, ContextType>;
+  start_dt?: Resolver<Maybe<ResolversTypes['DateTime']>, ParentType, ContextType>;
 }>;
 
 export type PersonResolvers<ContextType = any, ParentType extends ResolversParentTypes['Person'] = ResolversParentTypes['Person']> = ResolversObject<{
+  affiliations?: Resolver<Array<ResolversTypes['Organization']>, ParentType, ContextType>;
+  aka?: Resolver<Maybe<Array<Maybe<ResolversTypes['String']>>>, ParentType, ContextType>;
+  bio?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  dob?: Resolver<Maybe<ResolversTypes['Date']>, ParentType, ContextType>;
   education?: Resolver<Array<ResolversTypes['Organization']>, ParentType, ContextType>;
   emails?: Resolver<Maybe<Array<Maybe<ResolversTypes['String']>>>, ParentType, ContextType>;
   employment?: Resolver<Array<ResolversTypes['Organization']>, ParentType, ContextType>;
-  first_name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
   investments?: Resolver<Array<ResolversTypes['Asset']>, ParentType, ContextType>;
-  last_name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  middle_name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  user_accounts?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType>;
+  wiki_tag?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
@@ -420,12 +473,27 @@ export type PoliticalPositionResolvers<ContextType = any, ParentType extends Res
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   similar_positions?: Resolver<Array<ResolversTypes['PoliticalPosition']>, ParentType, ContextType>;
   summary?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  wiki_tag?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
 export type SimilarityInfoResolvers<ContextType = any, ParentType extends ResolversParentTypes['SimilarityInfo'] = ResolversParentTypes['SimilarityInfo']> = ResolversObject<{
   __resolveType: TypeResolveFn<null, ParentType, ContextType>;
   details?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+}>;
+
+export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = ResolversObject<{
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  owner?: Resolver<Array<ResolversTypes['Entity']>, ParentType, ContextType>;
+  platform?: Resolver<Array<ResolversTypes['MediaChannel']>, ParentType, ContextType>;
+  username?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type UserInfoResolvers<ContextType = any, ParentType extends ResolversParentTypes['UserInfo'] = ResolversParentTypes['UserInfo']> = ResolversObject<{
+  __resolveType: TypeResolveFn<null, ParentType, ContextType>;
+  details?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  user_since?: Resolver<Maybe<ResolversTypes['DateTime']>, ParentType, ContextType>;
 }>;
 
 export type Resolvers<ContextType = any> = ResolversObject<{
@@ -435,6 +503,8 @@ export type Resolvers<ContextType = any> = ResolversObject<{
   AssociationInfo?: AssociationInfoResolvers<ContextType>;
   Belief?: BeliefResolvers<ContextType>;
   Comment?: CommentResolvers<ContextType>;
+  Date?: GraphQLScalarType;
+  DateTime?: GraphQLScalarType;
   DistributionInfo?: DistributionInfoResolvers<ContextType>;
   EducationInfo?: EducationInfoResolvers<ContextType>;
   EmploymentInfo?: EmploymentInfoResolvers<ContextType>;
@@ -448,5 +518,7 @@ export type Resolvers<ContextType = any> = ResolversObject<{
   Person?: PersonResolvers<ContextType>;
   PoliticalPosition?: PoliticalPositionResolvers<ContextType>;
   SimilarityInfo?: SimilarityInfoResolvers<ContextType>;
+  User?: UserResolvers<ContextType>;
+  UserInfo?: UserInfoResolvers<ContextType>;
 }>;
 
--- models/ts/wikipedia/WikipediaApiArticle.ts
+++ models/ts/wikipedia/WikipediaApiArticle.ts
@@ -1,3 +1,5 @@
 export interface WikipediaApiArticle {
 	text: string;
+	title: string;
+	tag: string;
 }
